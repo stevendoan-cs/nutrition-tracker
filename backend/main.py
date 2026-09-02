a97e597f96805f900ec29c932aaf935c4ec6be88
Steven Doan <sdoan20@yahoo.ca>
2026-09-02 04:32:03 -0700

    Restrict CORS to deployed frontend origin

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://nutrition-tracker-azure-nine.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
